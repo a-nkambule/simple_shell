e91cb284c93a81d73948daa1dad6ba17f2014a17
Aviwe Nkambule <aviwenkambule@gmail.com>
2023-12-13 13:49:00 -0800

    project

--- builtin.c
+++ builtin.c
@@ -2,15 +2,15 @@
 
 /**
  * _myexit - exits the shell
- * @info: structure containing potential arguments
- * constant function prototype
- * Return: exits with given exit status
- * (0) if info.argv [0] != "exit"
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: exits with a given exit status
+ * (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
 int exitcheck;
-if (info->argv[1]) /*If ther is an exit argument*/
+if (info->argv[1])  /* If there is an exit arguement */
 {
 exitcheck = _erratoi(info->argv[1]);
 if (exitcheck == -1)
@@ -27,14 +27,15 @@ return (-2);
 info->err_num = -1;
 return (-2);
 }
+
 /**
  * _mycd - changes the current directory of the process
- * @info: structure containing potential arguments
- * used to maintain
- * constant function prototype
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
  * Return: Always 0
  */
 int _mycd(info_t *info)
+{
 char *s, *dir, buffer[1024];
 int chdir_ret;
 s = getcwd(buffer, 1024);
@@ -44,8 +45,8 @@ if (!info->argv[1])
 {
 dir = _getenv(info, "HOME=");
 if (!dir)
-chdir_ret = /* TODO: what should it be? */
-chdir_ret = chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+chdir_ret = /* TODO: what should this be? */
+chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
 else
 chdir_ret = chdir(dir);
 }
@@ -56,8 +57,8 @@ if (!_getenv(info, "OLDPWD="))
 _puts(s);
 _putchar('\n');
 return (1);
-_puts(_getenv(info, "OLDPWD=")),
-_putchar('\n');
+}
+_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
 chdir_ret = /* TODO: what should this be? */
 chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 }
@@ -66,7 +67,7 @@ chdir_ret = chdir(info->argv[1]);
 if (chdir_ret == -1)
 {
 print_error(info, "can't cd to ");
-_eputs(info->argv[1], _eputchar('\n');
+_eputs(info->argv[1]), _eputchar('\n');
 }
 else
 {
@@ -75,11 +76,12 @@ _setenv(info, "PWD", getcwd(buffer, 1024));
 }
 return (0);
 }
+
 /**
  * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: Always 0
+ *          constant function prototype.
+ *  Return: Always 0
  */
 int _myhelp(info_t *info)
 {
